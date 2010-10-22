b57a25131bac41ddd601e0cd1086a061fd71c495
Vicente Reig Rincón de Arellano <vicente.reig@justlanded.com>
2010-10-22 19:02:13 +0200

    Refs #4. WIP. I won't continue committing stuff while having a beer in my hand.

--- lib/lipsum_app.rb
+++ lib/lipsum_app.rb
@@ -1,22 +1,35 @@
+require 'rubygems'
 require 'thor'
-
+require 'lipsum'
 
 class LipsumApp < Thor
-  desc "paragraphs AMOUNT"
+  map 'p' => :paragraphs
+  map 'w' => :words
+  map 'l' => :lists
+  map 'b' => :bytes
+
+  desc "paragraphs AMOUNT", "Downloads an AMOUNT of paragraphs from Lipsum.com"
+  method_options  :start_with_lorem, :type => :boolean
   def paragraphs(amount=0)
-
+    amount.lipsum_paragraphs method_options if amount > 0
   end
 
-  desc "words AMOUNT"
+  desc "words AMOUNT", "Downloads an AMOUNT of words from Lipsum.com"
+  method_options  :start_with_lorem, :type => :boolean
   def words(amount=0)
+    amount.lipsum_words method_options if amount > 0
   end
 
-  desc "lists AMOUNT"
+  desc "lists AMOUNT", "Downloads an AMOUNT of lists from Lipsum.com"
+  method_options  :start_with_lorem, :type => :boolean
   def lists(amount=0)
+    amount.lipsum_lists method_options if amount > 0
   end
 
-  desc "bytes AMOUNT"
+  desc "bytes AMOUNT", "Downloads an AMOUNT of bytes from Lipsum.com"
+  method_options  :start_with_lorem, :type => :boolean
   def bytes(amount=0)
+    amount.lipsum_bytes method_options if amount > 0
   end
 
 end
